fe2051997825f9ffdd5f308e9aaf388d951741fa
Guionardo Furlan <guionardo.furlan@ambevtech.com.br>
2022-06-24 17:36:49 -0300

    Release

--- gs/__init__.py
+++ gs/__init__.py
@@ -1,5 +1,5 @@
 """Package data"""
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __tool_name__ = 'py-gstools'
 __description__ = 'Tool classes and functions for Guiosoft projects'
 __author__ = 'Guionardo Furlan'
